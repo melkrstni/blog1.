664a5df3e683d1ab315f008dbce74012c6890e8f
melkrstni <ameliakristiani27@gmail.com>
2020-01-26 21:57:44 +0700

    keduaaaaa

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('Layout/main')
+
+@section('title', 'RPL2')
+
+@section('container')
+ <div class="container">
+    <div class="row">
+        <div class="col-10">
+            <h1 class="mt-3">Home Page</h1>
+        </div>
+    </div>
+ </div>   
+@endsection
+
+    
